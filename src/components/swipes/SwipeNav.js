d3a99260220a4270ebeb9141733887b7a90368f0
kp1129 <katya.p.1129@gmail.com>
2020-02-04 18:47:35 -0700

    add swipe components

---
+++ src/components/swipes/SwipeNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SwipeNav = () => {
+    return (
+        <nav>
+            <button type='submit'>no</button>
+            <button type='submit'>yes</button>
+        </nav>
+    )
+}
+
+export default SwipeNav;
